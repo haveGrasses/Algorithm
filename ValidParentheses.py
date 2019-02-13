e1c8ed2c2488d62b8fc1a31564ba047a5c00200f
yuanyuan-w <w.y.yuan@foxmail.com>
2019-02-13 11:21:11 +0800

    add multiple solutions

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isValid(self, s: 'str') -> 'bool':
+    def isValid(self, s):
         stack = []  # using list as a stack
         for i in s:
             if i in '([{':
@@ -12,3 +12,34 @@ class Solution:
 
 # why using stack: nesting relations, the most recent found opener should be firstly paired
 # the first found opener should be lastly paired.
+
+
+class Solution2:
+	"""只考虑opener，push opener对应的closer而不是opener，简化判断条件"""
+    def isValid(self, s):
+        stack = []  # using list as a stack
+        for i in s:
+            if i == '(':
+                stack.append(')')
+            elif i == '[':
+                stack.append(']')
+            elif i == '{':
+                stack.append('}')
+            else:  # found closer
+                if stack == [] or stack.pop() != i:
+                    return False
+        return stack == []
+
+
+class Solution3:
+	""" Solution2将opener的每种情况进行if判断，当opener较多的时候有些冗长，可借助字典记录配对情况"""
+    def isValid(self, s):
+        stack = []  # using list as a stack
+        pair_dict = {'(': ')', '[': ']', '{': '}'}
+        for i in s:
+            if i in pair_dict.keys():
+                stack.append(pair_dict[i])
+            else:
+                if stack == [] or stack.pop() != i:
+                    return False
+        return stack == []
