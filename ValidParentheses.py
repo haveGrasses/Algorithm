715027831aae48181f21ca3c66e25e8c5c0b9991
yuanyuan-w <w.y.yuan@foxmail.com>
2019-02-15 22:07:24 +0800

    and another solution

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -12,6 +12,7 @@ class Solution:
 
 # why using stack: nesting relations, the most recent found opener should be firstly paired
 # the first found opener should be lastly paired.
+# 栈顶元素表示在嵌套层次关系中最近的需要匹配的元素
 
 
 class Solution2:
@@ -40,6 +41,20 @@ class Solution3:
             if i in pair_dict.keys():
                 stack.append(pair_dict[i])
             else:
-                if stack == [] or stack.pop() != i:
+                if stack == [] or stack.pop() != i:  
+                    return False
+        return stack == []
+
+
+class Solution4:
+    """ Solution2将opener的每种情况进行if判断，当opener较多的时候有些冗长，可借助字典记录配对情况"""
+    def isValid(self, s):
+        stack = []  # using list as a stack
+        pair_dict = {')': '(', ']': '[', '}': '{'}
+        for i in s:
+            if i in pair_dict.values():
+                stack.append(i)
+            else:
+                if stack == [] or stack.pop() != pair_dict[i]:  
                     return False
         return stack == []
