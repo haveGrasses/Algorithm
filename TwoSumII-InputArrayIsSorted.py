08f4ac5ff54d89bfe1adf0fdcf112a9c46412bbd
yuanyuan-w <w.y.yuan@foxmail.com>
2019-02-23 13:11:30 +0800

    add binary search method

--- TwoSumII-InputArrayIsSorted.py
+++ TwoSumII-InputArrayIsSorted.py
@@ -4,9 +4,9 @@ class Solution:
         lo, hi = 0, len(numbers) - 1
         while (numbers[lo] + numbers[hi] != target): 
             if numbers[lo] + numbers[hi]  < target:
-                lo = lo + 1
+                lo += 1
             else:
-                hi = hi - 1
+                hi -= 1
         return [lo+1, hi+1]
 
 
@@ -19,9 +19,9 @@ class Solution2:
             if tmp == target:
                 return [lo+1, hi+1]
             elif tmp > target:
-                hi = hi - 1
+                hi -= 1
             else:
-                lo = lo + 1
+                lo += 1
 
 
 class Solution3:
@@ -39,7 +39,17 @@ class Solution3:
 class Solution4:
     """ binary search method """
     def twoSum(self, numbers: 'List[int]', target: 'int') -> 'List[int]':
-        
+        for i in range(len(numbers)):
+            lo, hi = i+1, len(numbers)-1
+            while lo <= hi:
+                mid = lo + (hi - lo) // 2  # 1+(3-1) // 2
+                residual = target - numbers[i]
+                if numbers[mid] == residual:
+                    return i+1, mid+1
+                elif numbers[mid] > residual:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+
 solution = Solution()
-print('begin')
 print(solution.twoSum([2,7,11,15], 9))
