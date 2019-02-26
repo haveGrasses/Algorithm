3d98021a003967331eee58aab948da831e19894d
yuanyuan-w <w.y.yuan@foxmail.com>
2019-02-26 11:50:53 +0800

    need review

---
+++ 3LongestSubstringWithoutRepeatingCharacter.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = maxLength = 0
+        usedChar = {}
+        
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:  
+                # 限定start <= usedChar[s[i]]是为了不让start后退
+                start = usedChar[s[i]] + 1
+            else:
+                maxLength = max(maxLength, i - start + 1)
+
+            usedChar[s[i]] = i
+
+        return maxLength
