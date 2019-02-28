0d87a4f78c7604350f4db8b25610a08e7c59af5d
yuanyuan-w <w.y.yuan@foxmail.com>
2019-02-28 17:50:34 +0800

    single solution use heap

---
+++ 352DataStreamasDisjointIntervals.py
@@ -0,0 +1,45 @@
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+import heapq
+
+class SummaryRanges:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.intervals = []
+        
+
+    def addNum(self, val: int) -> None:
+        if val not in [x[0] for x in self.intervals]:  # avoid same key
+        # python3 doesn't allow heap to have the same value key
+            heapq.heappush(self.intervals, (val, Interval(val, val)))
+
+
+    def getIntervals(self) -> List[Interval]:
+        mergedIntervals = []
+        
+        while self.intervals:
+            idx, cur = heapq.heappop(self.intervals)
+            if not mergedIntervals:  # make a initial add
+                mergedIntervals.append((idx, cur))
+            else:
+                _, prev = mergedIntervals[-1]  # fetch last one to compare
+                if prev.end + 1 >= cur.start:
+                    prev.end = max(prev.end, cur.end)
+                else:
+                    mergedIntervals.append((idx, cur))
+        self.intervals = mergedIntervals
+        return list(map(lambda i: i[1], mergedIntervals))
+        
+
+
+# Your SummaryRanges object will be instantiated and called as such:
+# obj = SummaryRanges()
+# obj.addNum(val)
+# param_2 = obj.getIntervals()
