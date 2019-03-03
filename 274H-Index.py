00e717ab39f40dd050bd908847ecb9353681080b
yuanyuan-w <w.y.yuan@foxmail.com>
2019-03-03 22:26:13 +0800

    add explanation

--- 274H-Index.py
+++ 274H-Index.py
@@ -8,12 +8,14 @@ class Solution(object):
         freq = [0] * (length+1)
 
         # find freq of every citation, bigger than length is viewed as length
+        # use freq as a dict, the index is citation number, the value is freqency
         for c in citations:
             if c > length:
                 freq[length] += 1
             else:
                 freq[c] += 1
 
+        # calculate H-index
         res = 0
         for i in reversed(range(length+1)):
             res += freq[i]
