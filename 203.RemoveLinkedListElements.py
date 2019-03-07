19d9b06ff11b05a3d82fe2d8f2feb4255d13122c
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-03-07 20:10:30 +0800

    use recursion

---
+++ 203.RemoveLinkedListElements.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        if not head:
+            return
+        head.next = self.removeElements(head.next, val)
+        return head if head.val != val else head.next
+        
