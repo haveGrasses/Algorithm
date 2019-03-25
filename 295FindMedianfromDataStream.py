454952c54d9414d5bf45768890769226d5093185
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-03-25 11:21:17 +0800

    add comments

--- 295FindMedianfromDataStream.py
+++ 295FindMedianfromDataStream.py
@@ -14,9 +14,13 @@ class MedianFinder(object):
         :type num: int
         :rtype: None
         """
-        # samll, large = self.heaps
-        heappush(self.samll, -heappushpop(self.large, num))
-        if len(self.large) < len(self.samll):
+        # use negative value in samll to keep big value in the front 
+        # caz we need to fetch the first one in samll to calc the median value 
+        # (because it is a data stream, we can onluy access its first value not its last one, the size is changing)
+        # and the first one must be the biggest one in samll, so use negative value to keep big one ahead in a heap
+        
+        heappush(self.samll, -heappushpop(self.large, num))  # add new num to large, pop the samllest one in large to samll
+        if len(self.large) < len(self.samll):  # keep the size of large always equal or only one bigger than samll
             heappush(self.large, -heappop(self.samll))
 
     def findMedian(self):
@@ -26,4 +30,4 @@ class MedianFinder(object):
         # samll, large = self.heaps
         if len(self.large) > len(self.samll):
             return self.large[0]
-        return (self.large[0] - self.samll[0]) / 2
+        return (self.large[0] - self.samll[0]) / 2  # pay attention to the minus, nums in samll is kept negative
