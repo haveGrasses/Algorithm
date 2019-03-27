ea1a81396b00f794500654a1b74ed9feebbd324f
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-03-27 22:45:12 +0800

    fix bug

--- 142LinkedListCycleII.py
+++ 142LinkedListCycleII.py
@@ -19,4 +19,35 @@ class Solution:
             slow = slow.next
             fast = fast.next
         return slow
+    
+# -------------- fix bug ----------------- #
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        phead = head
+        if not phead or not phead.next:
+            return 
+        slow, fast = phead.next, phead.next.next
+        if not slow or not fast:  # added
+            return
+        while slow != fast:
+            if not fast or not fast.next:
+                return 
+            slow = slow.next
+            fast = fast.next.next
+        # fast meet slow
+        fast = phead
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
         
