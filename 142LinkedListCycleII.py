3d6e397c758f18cc8a2cf74a6b65f810a61bffb3
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-03-27 23:32:55 +0800

    add a concise solution

--- 142LinkedListCycleII.py
+++ 142LinkedListCycleII.py
@@ -51,3 +51,28 @@ class Solution(object):
             fast = fast.next
         return slow
         
+# ---------- a preferable optimal one --------------- #
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                fast = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return
+        
