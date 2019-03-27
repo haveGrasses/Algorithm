89fdac596bb00f1ab5b7d8df7208c391af8a133b
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-03-27 22:30:05 +0800

    AC but not bug-free?

---
+++ 141LinkedListCycle.py
@@ -0,0 +1,22 @@
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    def EntryNodeOfLoop(self, pHead):
+        phead = pHead
+        if not phead or not phead.next:
+            return 
+        slow, fast = phead.next, phead.next.next
+        if not slow or not fast:
+            return
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next.next
+        # fast meet slow
+        fast = phead
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+        
