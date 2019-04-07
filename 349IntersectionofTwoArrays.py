dd9f9ec1df556594eee57a4564862ab8c98315e3
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-04-07 11:29:54 +0800

    add several different solutions

--- 349IntersectionofTwoArrays.py
+++ 349IntersectionofTwoArrays.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    """ 44.28% """
+    """ 81% """
     def intersection(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -14,4 +14,17 @@ class Solution(object):
                 res.append(i)
                 nums1_set.remove(i)
         return res
+    
+    # same as:
+    class Solution(object):
+    def intersection(self, nums1, nums2):
+        nums1_set = set(nums1)
+        nums2_set = set(nums2)
+        return list(nums1_set.intersection(nums2_set))  # make sure the return is a list in py3
                 
+    
+    # a one line solution
+    def intersection(self, nums1, nums2):
+        """ 81% """
+        return list(set(nums1) & set(nums2))
+    
