e62680f34bb510ef2e439ee59bd0794194353938
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-04-07 15:14:42 +0800

    add a two pointer solution

--- 350.IntersectionofTwoArraysII.py
+++ 350.IntersectionofTwoArraysII.py
@@ -21,3 +21,28 @@ class Solution:
         
         return res
     
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        """ two pointer solution 
+        when nums1 and nums2 are very large, it is not impossible to make a set of them,
+        so we use two pointer to compare elements in them and append the repeated one to res
+        """
+        # sort
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        
+        # traverse and  compare
+        i1, i2 = 0, 0
+        n1, n2 = len(nums1), len(nums2)
+        res = []
+        while i1 < n1 and i2 < n2:
+            if nums1[i1] == nums2[i2]:
+                res.append(nums1[i1])
+                i1 += 1
+                i2 += 1
+            elif nums1[i1] > nums2[i2]:
+                i2 += 1
+            else:  # nums1[i1] < nums2[i2]
+                i1 += 1
+        
+        return res
+    
