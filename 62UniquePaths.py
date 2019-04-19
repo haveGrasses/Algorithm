67271eb7e80d3ee167c3b9a5dd5239d48c174596
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-04-19 13:56:45 +0800

    simple dp method

---
+++ 62UniquePaths.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """ using dp, seemingly not that horrible
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        
+        dp = [[1]*n]*m  # m row n col
+        # the initial value is 1, cause dp[0][j] = dp[i][0]
+        # the loop from index 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+        
