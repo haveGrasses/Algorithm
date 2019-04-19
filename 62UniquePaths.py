3633a9309e2762e61da7db676a8b971004136a3c
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-04-19 13:59:41 +0800

    polishment

--- 62UniquePaths.py
+++ 62UniquePaths.py
@@ -5,12 +5,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
-        dp = [[1]*n]*m  # m row n col
+        # dp = [[1 for x in range(n)] for x in range(m)]  # others
+        dp = [[1]*n]*m  # m row n col, looks cleaner than above, faster?
         # the initial value is 1, cause dp[0][j] = dp[i][0]
         # the loop from index 1
         for i in range(1, m):
             for j in range(1, n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        # return dp[-1][-1]  # others
         return dp[m-1][n-1]
         
