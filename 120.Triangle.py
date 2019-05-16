e7fec3c005ccf2fc792f51cbd9bd96b33b6af308
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-05-16 21:17:03 +0800

    bottom up 90% dp

--- 120.Triangle.py
+++ 120.Triangle.py
@@ -6,7 +6,8 @@ class Solution:
         n = len(triangle)
         dp = triangle[n-1]
         for i in range(n-2, -1, -1):  # n-2:last layer do not need to optimize
-            for j in range(i+1):  # node
-                dp[j] = min(dp[j], dp[j+1]) + triangle[i][j]
+            for j in range(i+1):  # node on this layer
+                dp[j] = min(dp[j], dp[j+1]) + triangle[i][j]  # compare its two childs, optimize current one
         print(dp)
-        return dp[0]  # bottom-up方法，优化到最上层，加上第一个元素后的最小值位于第一个索引
+        return dp[0]  # bottom-up method, optimize to the top layer, the minimun sum 
+        # locates in the first indec，优化到最上层，加上第一个元素后的最小值位于第一个索引
