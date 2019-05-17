6552f0588ac0cba1d08c192d64936dfef3beb081
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-05-17 18:17:32 +0800

    dp fibonacci

--- 70.ClimbingStairs.py
+++ 70.ClimbingStairs.py
@@ -11,3 +11,14 @@ class Solution:
         for i in range(2, n):
             dp[i] = dp[i-1] + dp[i-2]
         return dp[-1]
+
+    
+class Solution:
+    """ a more space efficient method """
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        last2, last1 = 1, 2
+        for i in range(2, n):
+            last2, last1 = last1, last1 + last2
+        return last1
