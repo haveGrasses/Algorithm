891954f5fb9cf98d5c44d0054fbe9b1712d58219
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-05-26 22:17:52 +0800

    bottom up

--- SU_0-1knapsack.py
+++ SU_0-1knapsack.py
@@ -101,3 +101,22 @@ w = [1, 2, 3]
 v = [6, 10, 12]
 print(s.knapsack(w, v, 5))
 
+
+class Solution:
+    """ bottom up, CAUTION: a false solution! """
+    def knapsack(self, w, v, C):
+        n = len(w)
+        dp = [[0 for _ in range(C+1)] for _ in range(n)]
+        
+        for j in range(C+1):
+            dp[0][j] = v[0] if j >= w[0] else 0
+        
+        for i in range(1, n):
+            for j in range(C+1): 
+                res = dp[i-1][j]  # dp[i][j] = dp[i-1][j]
+                if j - w[i] >= 0:
+                    res = max(res, v[i] + dp[i-1][j-w[i]])  # dp[i][j] = max(dp[i][j], v[i] + dp[i-1][j-w[i]])
+                dp[i][j] = res
+        return dp[-1][-1]
+
+
