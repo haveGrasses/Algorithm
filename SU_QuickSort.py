2d2794aa647bcd751a7e51e3e91fda8045fba309
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-06-04 23:13:51 +0800

    partition

--- SU_QuickSort.py
+++ SU_QuickSort.py
@@ -3,10 +3,10 @@ def quickSort(nums):
 
 
 def qSort(nums, lo, hi):
-    if lo >= hi:
+    if lo >= hi:  # !important
         return 
     
-    p = partition(nums, lo, hi)
+    p = partition2(nums, lo, hi)
     qSort(nums, lo, p-1)
     qSort(nums, p+1, hi)
     
@@ -16,10 +16,6 @@ def partition(nums, lo, hi):
     :return: index 
     将nums分成两部分，一部分在index的左边，它们全都小于等于index，一部分在index的右边，它们全都大于index
     """
-    
-    def swap(nums, i, j):
-        nums[i], nums[j] = nums[j], nums[i]
-    
     swap(nums, lo, lo+(hi-lo)//2) 
     i, j = lo + 1, hi
     while True:
@@ -32,3 +28,19 @@ def partition(nums, lo, hi):
         swap(nums, i, j)
     swap(nums, lo, j)
     return j
+
+
+def swap(nums, i, j):
+        nums[i], nums[j] = nums[j], nums[i]
+        
+        
+def partition2(array, l, r):
+    """ 只用一个指针，不用while循环，highly recommended! """
+    x = array[r]
+    i = l
+    for j in range(l, r):
+        if array[j] <= x:
+            swap(array, i, j)
+            i += 1
+    swap(nums, i, r)
+    return i
