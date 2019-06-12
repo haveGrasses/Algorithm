db0513274a267acbf8ab55b758ab5a7afefe2a22
haveGrasses <35300364+haveGrasses@users.noreply.github.com>
2019-06-12 13:14:27 +0800

    binary search

---
+++ SU_binarySearch.py
@@ -0,0 +1,20 @@
+def binary_search(arr, target):
+    l, r = 0, len(arr) - 1
+    while l <= r:
+        mid = l + (r - l) // 2
+        if arr[mid] < target:
+            l = mid + 1
+        elif arr[mid] > target:
+            r = mid - 1
+        else:
+            return mid
+    return -1
+
+
+arr = [3, 9, 28, 67, 12, 45]
+arr.sort()
+print(arr)
+print(binary_search_while(12, arr))
+print(binary_search_while(3, arr))
+print(binary_search_while(9, arr))
+print(binary_search_while(99, arr))
