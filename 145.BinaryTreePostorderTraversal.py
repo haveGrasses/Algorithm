ec134691845be791e41f749d3c23e698e80de441
haveGrasses <w.y.yuan@foxmail.com>
2019-07-17 11:48:37 +0800

    postorder

---
+++ 145.BinaryTreePostorderTraversal.py
@@ -0,0 +1,45 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    """ recursion """
+    def __init__(self):
+        self.res = []
+        
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        self.postorderTraversal(root.left)
+        self.postorderTraversal(root.right)
+        self.res.append(root.val)
+        return self.res
+    
+class Solution:
+    """ non-recursion 
+    和preorder思路一样，只需要再借助一个栈来实现反序就行！！！妙！
+    后续遍历反序前序遍历是过程和结果都要反
+    """
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        output, res, stack = [], [], []
+        stack.append(root)
+        while stack:
+            curNode = stack.pop()
+            res.append(curNode.val)
+            
+            if curNode.left:  # 这里preorder是先右后左
+                stack.append(curNode.left)
+                
+            if curNode.right:
+                stack.append(curNode.right)
+            
+        
+        while res:
+            output.append(res.pop())
+        return output
