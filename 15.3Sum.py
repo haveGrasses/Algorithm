ea45b85f4f5f32fe12219bc3132efe62fa38bb14
haveGrasses <w.y.yuan@foxmail.com>
2019-07-20 10:46:53 +0800

    remove redundant file

--- 15.3Sum.py
+++ 15.3Sum.py
@@ -1,5 +1,35 @@
+from typing import List
+
+
+# this ans is preferred
 class Solution:
-    """"""
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            # the if condition filters same number
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            residual = 0 - nums[i]
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                if nums[l] + nums[r] == residual:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    # the following two while is very important, caz it avoids same answers
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+                elif nums[l] + nums[r] > residual:
+                    r -= 1
+                else:
+                    l += 1
+        return res
+
+
+class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
