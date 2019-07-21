bb9b061eb126aa02bce372d89ecee7808cfab114
haveGrasses <w.y.yuan@foxmail.com>
2019-07-21 21:55:31 +0800

    extensions

--- 295FindMedianfromDataStream.py
+++ 295FindMedianfromDataStream.py
@@ -16,10 +16,11 @@ class MedianFinder(object):
         """
         # use negative value in samll to keep big value in the front 
         # caz we need to fetch the first one in samll to calc the median value 
-        # (because it is a data stream, we can onluy access its first value not its last one, the size is changing)
+        # (because it is a data stream, we can only access its first value not its last one, the size is changing)
         # and the first one must be the biggest one in samll, so use negative value to keep big one ahead in a heap
         
-        heappush(self.samll, -heappushpop(self.large, num))  # add new num to large, pop the samllest one in large to samll
+        heappush(self.samll, -heappushpop(self.large, num))
+        # add new num to large, pop the samllest one in large to samll
         if len(self.large) < len(self.samll):  # keep the size of large always equal or only one bigger than samll
             heappush(self.large, -heappop(self.samll))
 
@@ -30,7 +31,8 @@ class MedianFinder(object):
         # samll, large = self.heaps
         if len(self.large) > len(self.samll):
             return self.large[0]
-        return (self.large[0] - self.samll[0]) / 2  # pay attention to the minus, nums in samll is kept negative from its original values
+        return (self.large[0] - self.samll[0]) / 2
+        # pay attention to the minus, nums in samll is kept negative from its original values
 
  
 class MedianFinder(object):
@@ -66,6 +68,7 @@ class MedianFinder(object):
 
 # TODO
 def findMedian(arr):
+    # 见 215
     pass
 
 
