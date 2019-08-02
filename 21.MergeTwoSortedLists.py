c3395e438e629291a4faf41ba5cb1f352dade194
haveGrasses <w.y.yuan@foxmail.com>
2019-08-02 14:54:06 +0800

    commments

--- 21.MergeTwoSortedLists.py
+++ 21.MergeTwoSortedLists.py
@@ -28,9 +28,13 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        """ 递归：先用if-else确定一个小的在前面，假如是l1，则l1.next=用同样的函数对l1.next 和 l2 进行处理，
+        然后直接返回这一个条件中确定的最小的那个node，也就是l1！
+        直接返回很重要，写这样的递归到后面很容易就会不知道最后到底要返回啥！以及在哪里返回！
+        """
         if not l1 or not l2:
             return l1 or l2
-        # 核心就是确定一个最小的在前面，后面的再递归获得，所以只要确定一个在前面的l就行
+        # 核心就是确定一个最小的在前面，后面的再递归获得，所以只要确定一个在前面的l就行，一个if-else搞定
         if l1.val < l2.val:
             l1.next = self.mergeTwoLists(l1.next, l2)
             return l1
@@ -39,7 +43,9 @@ class Solution:
             return l2
 
     def mergeTwoLists2(self, l1, l2):
-        """ non-recursive """
+        """ non-recursive: 初始一个dummyHead，初始一个cur从dummyHead开始，当l1和l2都不为空时，用if-else确定将
+        谁加到cur的后面，加了之后相应对应链表的头，同时要后移cur到新加的节点上
+        """
         if not l1 or not l2:
             return l1 or l2
         dummyHead = ListNode(-1)
@@ -56,51 +62,29 @@ class Solution:
         return dummyHead.next
 
 
-# 1 ->2->4, 1->3->4
-l1 = ListNode(1)
-l2 = ListNode(2)
-l4 = ListNode(4)
-l1.next = l2
-l2.next = l4
+def data():
+    # 1 ->2->4, 1->3->4
+    l1 = ListNode(1)
+    l2 = ListNode(2)
+    l4 = ListNode(4)
+    l1.next = l2
+    l2.next = l4
 
-l11 = ListNode(1)
-l33 = ListNode(3)
-l44 = ListNode(4)
-l11.next = l33
-l33.next = l44
+    l11 = ListNode(1)
+    l33 = ListNode(3)
+    l44 = ListNode(4)
+    l11.next = l33
+    l33.next = l44
 
-print(l1.toString())
-print(l11.toString())
+    # print(l1.toString())
+    # print(l11.toString())
+    return l1, l11
+
+
+l1, l11 = data()
 print(Solution().mergeTwoLists(l1, l11).toString())
-print('-----------')
-print(l1.toString())
-print(l11.toString())
-# 1--> 2--> 4
-# 1--> 3--> 4
-# 上面的函数会改变l1和l2的取值
+# 上面这个函数会改变l1和l2的取值为：
 # 1--> 2--> 3--> 4--> 4
 # 1--> 1--> 2--> 3--> 4--> 4
-
-# l1 = ListNode(1)
-# l2 = ListNode(2)
-# l3 = ListNode(3)
-# l4 = ListNode(4)
-# l44 = ListNode(4)
-# l1.next = l2
-# l2.next = l3
-# l3.next = l4
-# l4.next = l44
-
-# l11 = ListNode(1)
-# l12 = ListNode(1)
-# l22 = ListNode(2)
-# l33 = ListNode(3)
-# l444 = ListNode(4)
-# l45 = ListNode(4)
-# l11.next = l12
-# l12.next = l22
-# l22.next = l33
-# l33.next = l444
-# l444.next = l45
+l1, l11 = data()
 print(Solution().mergeTwoLists2(l1, l11).toString())
-
