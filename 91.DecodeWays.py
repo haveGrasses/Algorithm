cdd8bdcac181c7e833ade6933853b80e4ec2ad97
haveGrasses <w.y.yuan@foxmail.com>
2019-08-06 09:48:42 +0800

    update

--- 91.DecodeWays.py
+++ 91.DecodeWays.py
@@ -9,7 +9,7 @@ class Solution:
         for i in range(2, n+1):
             if s[i-1] != '0':  # current one valid:
                 dp[i] = dp[i-1]
-            if 10 <= int(s[i-1]) + int(s[i-2]) * 10 <=26:
+            if 10 <= int(s[i-1]) + int(s[i-2]) * 10 <= 26:
                 dp[i] += dp[i-2]  # 包含 dp[i] = dp[i-1] + dp[i-2] 和 dp[i] = dp[i-2]两种情况
         return dp[-1]
 
@@ -25,7 +25,7 @@ class Solution:
             dp = 0
             if s[i-1] != '0':  # current one valid:
                 dp = dp1
-            if 10 <= int(s[i-1]) + int(s[i-2]) * 10 <=26:
+            if 10 <= int(s[i-1]) + int(s[i-2]) * 10 <= 26:
                 dp += dp2  # 包含 dp = dp1 + dp2 和 dp = dp2 两种情况
             dp2 = dp1  # 现更新dp2的值！因为dp2依赖没有更新的dp1的值
             dp1 = dp
@@ -38,7 +38,7 @@ class Solution:
     """
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        if n == 0 or s[0] == 0:
+        if n == 0 or s[0] == '0':
             return 0
         dp = [0 for _ in range(n + 1)]
         dp[n] = 1
@@ -50,4 +50,24 @@ class Solution:
                 dp[i] = dp[i + 1] + dp[i + 2]  # 一个数和两个数都valid
             else:
                 dp[i] = dp[i + 1]  # 仅当前一个数valid
+        return dp[0]
+
+    # 其实上面for里面那个if-if-else也可以写成这样：
+    # if s[i] != 0:
+    #     dp[i] = dp[i+1]
+    # if 10 <= int(s[i]) * 10 + int(s[i + 1]) <= 26:
+    #     dp[i] += dp[i+2]
+
+    def numDecodings2(self, s: str) -> int:
+        n = len(s)
+        if n == 0 or s[0] == '0':
+            return 0
+        dp = [0 for _ in range(n + 1)]
+        dp[n] = 1
+        dp[n - 1] = 0 if s[n - 1] == '0' else 1
+        for i in range(n - 2, -1, -1):
+            if s[i] != '0':
+                dp[i] = dp[i+1]
+            if 10 <= int(s[i]) * 10 + int(s[i + 1]) <= 26:
+                dp[i] += dp[i+2]
         return dp[0]
