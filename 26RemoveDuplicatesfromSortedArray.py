c097f6dab3c6f030158833ad50f0ab73a2712b4d
haveGrasses <w.y.yuan@foxmail.com>
2019-08-07 17:47:52 +0800

    add changes

--- 26RemoveDuplicatesfromSortedArray.py
+++ 26RemoveDuplicatesfromSortedArray.py
@@ -1,13 +1,24 @@
 class Solution:
+    """ idx 所指的位置是上一个unique的数，每次发现unique的数需要在idx+1的位置插入"""
     def removeDuplicates(self, nums: List[int]) -> int:
         if len(nums) < 2:
             return len(nums)
-        id = 0
+        idx = 0
         for i in range(1, len(nums)):
             if nums[i] != nums[i-1]:
-                id += 1
-                nums[id] = nums[i]
-        return id+1
-               
-        
-        
+                idx += 1
+                nums[idx] = nums[i]
+        return idx+1
+
+
+class Solution:
+    """ idx 所指的位置是下一个unique的数，每次发现unique的数需要在idx的位置插入 """
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return len(nums)
+        idx = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[idx] = nums[i]
+                idx += 1
+        return idx
