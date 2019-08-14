67fa7bdaabb99f81f79ab27af35baa485aa6888c
haveGrasses <w.y.yuan@foxmail.com>
2019-08-14 15:55:45 +0800

    renew

--- JZ_2.ReplaceSpace.py
+++ JZ_2.ReplaceSpace.py
@@ -1,5 +1,8 @@
 class Solution:
-    # s 源字符串
+    """ 统计空格的个数，定义一个新list，长度为原字符串的长度+空格个数*2，
+    两个指针i，j从后向前扫，依次向新数组补数
+    为啥要从后向前扫：
+    """
     def replaceSpace(self, s):
         numSpace = 0
         for i in s:
@@ -22,5 +25,29 @@ class Solution:
                 i -= 1
         return ''.join(newStr)
 
+    def replaceSpace2(self, s):
+        """从前往后扫也没毛病"""
+        numSpace = 0
+        for i in s:
+            if i == ' ':
+                numSpace += 1
+        newStr = ['' for _ in range(len(s)+numSpace*2)]
+        i, j = 0, 0
+        while i < len(s):
+            if s[i] != ' ':
+                newStr[j] = s[i]
+                i += 1
+                j += 1
+            else:
+                newStr[j] = '%'
+                j += 1
+                newStr[j] = '2'
+                j += 1
+                newStr[j] = '0'
+                j += 1
+                i += 1
+        return ''.join(newStr)
+
 
 print(Solution().replaceSpace('u jko l p'))
+
