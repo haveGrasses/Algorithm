a96f600cab32558c73b2b455ae3e478455861e61
haveGrasses <w.y.yuan@foxmail.com>
2019-09-01 16:22:28 +0800

    revise

--- 154FindMinimuminRotatedSortedArrayII.py
+++ 154FindMinimuminRotatedSortedArrayII.py
@@ -10,8 +10,20 @@ class Solution:
                 hi = mid   # todo: not mid-1, why: nums[mid] may be the pivot value
             else:  # nums[mid] == nums[hi] == nums[lo]
                 # the following three line can be deleted, but it ensures the right index of pivot value
-                if nums[hi-1] < nums[hi]:  # pivot encountered
-                    lo = hi-1  # pivot
-                    break
-                hi -= 1
+                if nums[hi-1] > nums[hi]:  # pivot encountered
+                    lo = hi  # pivot
+                    break  # 注1
+                hi -= 1  # When num[mid] == num[hi], we couldn't sure the position of minimum in mid's left or right, so just let upper bound reduce one.
         return nums[lo]
+
+
+print(Solution().findMin([1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1]))
+# eg
+# 2,2,2,0,1,2: nums[mid] = 2, nums[lo] = 2, mums[hi] = 2, minimal = 0, right to mid
+# 2,0,1,2,2,2,2: nums[mid] = 2, nums[lo] = 2, mums[hi] = 2, minimal = 1, left to mid
+# so when num[mid] == num[hi], we couldn't sure the position of minimum in mid's left or right
+
+# 注1：和else里面三行一样的功能
+# if nums[hi-1] <= nums[hi]:  #  和上面三行一样的效果
+#     lo = hi - 1  # pivot
+#     break
