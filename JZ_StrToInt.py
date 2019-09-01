a31b02ea89da8401148728b3bdc5490b7abdd0f3
haveGrasses <w.y.yuan@foxmail.com>
2019-09-01 17:35:13 +0800

    ok

--- JZ_StrToInt.py
+++ JZ_StrToInt.py
@@ -7,9 +7,9 @@ class Solution:
         ret = 0
         for i in range(len(s)):
             c = s[i]
-            if i == 0 and (c == '+' or c == '-'):
+            if i == 0 and (c == '-' or c == '+'):
                 continue
             if c < '0' or c > '9':  # illegal input: '$' < '0', 'a' > '9'
                 return 0
             ret = ret * 10 + intmap[c]
-        return -1*ret if isNegative else ret
+        return -ret if isNegative else ret
