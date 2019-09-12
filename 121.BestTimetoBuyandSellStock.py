1ef15c905cc9bd0d30ccffcee7e1791e5646cef0
haveGrasses <w.y.yuan@foxmail.com>
2019-09-12 21:27:15 +0800

    add

--- 121.BestTimetoBuyandSellStock.py
+++ 121.BestTimetoBuyandSellStock.py
@@ -2,13 +2,28 @@ from typing import List
 
 
 class Solution:
-    """ dp"""
+    """ dp
+    profit: 如果在该点卖出所能达到的最大收益
+    得到这个最大收益，需要知道在当前该点的最小cost
+    profit 本来是一个一维数组，记录的是每个点的最大收益，相应地需要知道该点之前的最小cost，所以需要记录这个cost
+    """
     def maxProfit(self, prices: List[int]) -> int:
         if not prices:
             return 0
-        cost, profit = prices[0], 0  # initialize, note profit = 0 for the first time 
+        cost, profit = prices[0], 0  # initialize, note profit = 0 for the first time
         for i in range(len(prices)):
-            cost = min(cost, prices[i])
-            profit = max(profit, prices[i] - cost)
+            profit = max(profit, prices[i] - cost)  # 这里的cost是i点之前的最小cost
+            cost = min(cost, prices[i])  # 在更新完profit后更新cost，把当前点考虑进来
         return profit
 
+
+# 看一下最原始的暴力解法，结果当然是LTE，但是有必要对比想一下是如何优化为dp的思路的：
+class Solution1:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        buy = 0
+        while buy < len(prices):
+            for sell in range(buy, len(prices)):
+                res = max(res, prices[sell] - prices[buy])
+            buy += 1
+        return res
