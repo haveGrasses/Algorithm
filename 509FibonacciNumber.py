808d9aca6723682145e17c8a4f5ff2211f894316
haveGrasses <w.y.yuan@foxmail.com>
2019-09-16 17:43:06 +0800

    revise

--- 509FibonacciNumber.py
+++ 509FibonacciNumber.py
@@ -1,7 +1,27 @@
+# LC
 class Solution(object):
     def fib(self, N):
         a, b = 0, 1
         for i in range(N):
             a, b = b, a+b
         return a
-        
+
+# JZ
+class Solution:
+    """ 以前很喜欢的，现在2019-09-16很不喜欢的写法 """
+    def Fibonacci(self, n):
+        dp1, dp2 = 0, 1
+        for i in range(n):  # 这里的循环次数
+            dp1, dp2 = dp2, dp1+dp2
+        return dp1  # 还有返回的是第一个...
+
+
+class Solution:
+    """ dp模板式的写法 """
+    def Fibonacci(self, n):
+        if n <= 1:
+            return n
+        dp1, dp2 = 0, 1
+        for i in range(2, n+1):
+            dp1, dp2 = dp2, dp1+dp2
+        return dp2
