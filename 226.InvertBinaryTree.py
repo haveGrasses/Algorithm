7fd521440a5293d65d5f781068de4a3b5b1d6b4e
haveGrasses <w.y.yuan@foxmail.com>
2019-09-20 11:41:18 +0800

    iterative

--- 226.InvertBinaryTree.py
+++ 226.InvertBinaryTree.py
@@ -7,11 +7,26 @@ class TreeNode:
 
 
 class Solution:
+    """ self """
     def invertTree(self, root: TreeNode) -> TreeNode:
+        """ recursion """
         if not root:
             return None
         left = self.invertTree(root.left)
         right = self.invertTree(root.right)
         root.left = right
         root.right = left
+        return root
+
+
+class Solution2:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        """ iterative, 其实就是层序遍历 """
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            if node:
+                node.left, node.right = node.right, node.left
+                queue.append(node.left)
+                queue.append(node.right)
         return root
