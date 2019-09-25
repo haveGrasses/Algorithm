8038ed736e6ad028321e8a4e6db2acdbf8b93213
haveGrasses <w.y.yuan@foxmail.com>
2019-09-25 21:46:20 +0800

    pointer

---
+++ 283.MoveZeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    """ 记录下一个不为0的元素插入的位置即可，剩下的位置填0 """
+    def moveZeroes(self, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        non_zero_pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_pos] = nums[i]
+                non_zero_pos += 1
+        while non_zero_pos < len(nums):
+            nums[non_zero_pos] = 0
+            non_zero_pos += 1
