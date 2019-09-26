382122241fbe7c4c9854439032c8157f0aaea3cc
haveGrasses <w.y.yuan@foxmail.com>
2019-09-26 19:00:19 +0800

    update

--- 56MergeIntervals.py
+++ 56MergeIntervals.py
@@ -1,8 +1,11 @@
 # Definition for an interval.
-# class Interval:
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+from typing import List
+
+
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
 
 class Solution:
     def merge(self, intervals: List[Interval]) -> List[Interval]:
@@ -13,4 +16,18 @@ class Solution:
             else:
                 ret.append(i)
         return ret
-                
+
+for i in Solution().merge([Interval(1, 3), Interval(4, 6), Interval(4, 5), Interval(4, 7)]):
+    print(i.start, i.end)
+
+
+class Solution:
+    """ 新版没有Interval class的设定了，解法还是一样的 """
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ret = []
+        for i in sorted(intervals, key=lambda x: x[0]):
+            if ret and ret[-1][1] >= i[0]:
+                ret[-1][1] = max(ret[-1][1], i[1])
+            else:
+                ret.append(i)
+        return ret
