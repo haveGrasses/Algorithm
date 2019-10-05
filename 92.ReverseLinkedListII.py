40f65d10b0163e00f92976da14bbe0bcd21a6abe
haveGrasses <w.y.yuan@foxmail.com>
2019-10-05 22:06:51 +0800

    bms

--- 92.ReverseLinkedListII.py
+++ 92.ReverseLinkedListII.py
@@ -4,7 +4,8 @@ class ListNode:
         self.val = x
         self.next = None
 
-class Solution:
+class Solution0:
+    """ bms """
     def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
         dummyHead = ListNode(-1)
         dummyHead.next = head
@@ -28,3 +29,28 @@ class Solution:
             cur = nxt
             k -= 1
         return prev, cur, head
+
+
+class Solution:
+    """ 差不多的思路 """
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+        dummyHead = ListNode(-1)
+        dummyHead.next = head
+        prev = dummyHead
+        count = 0
+        while count < m - 1:
+            prev = prev.next
+            count += 1
+        # prev是前面保持不动的结尾
+
+        tmp = prev.next  # 记录这个位置，这是反转后的结尾，要去接后面没反转的位置，相当于反转部分的head
+        cur = prev.next
+        start = None
+        for _ in range(n - m + 1):
+            nxt = cur.next
+            cur.next = start
+            start = cur
+            cur = nxt
+        prev.next = start  # start是反转部分反转后的头节点，prev是前面保持不动的结尾，连接起来
+        tmp.next = cur  # tmp是反转部分反转后的尾节点，cur是后面没反转的头节点
+        return dummyHead.next
